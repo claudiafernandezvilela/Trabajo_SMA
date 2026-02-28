a2793acf1c2c015c5354ab8ebc504bda26a38b26
sofiagarciap <sofia.garciap@udc.es>
2026-02-28 12:38:25 +0100

    Puertas

--- Assets/Free Wood Door Pack/Script/AbrirPuerta.cs
+++ Assets/Free Wood Door Pack/Script/AbrirPuerta.cs
@@ -8,10 +8,14 @@ public class AbrirPuerta : MonoBehaviour
     public float anguloAbierta = 150f;
     public BoxCollider colliderBloqueo;
     public NavMeshObstacle navMeshObstacle;
+
     private bool abierta = false;
     private bool jugadorCerca = false;
+    private bool agenteCerca = false;
     private Quaternion rotacionCerrada;
     private Quaternion rotacionAbierta;
+
+    public bool EstaAbierta => abierta;
 
     void Start()
     {
@@ -21,12 +25,9 @@ public class AbrirPuerta : MonoBehaviour
 
     void Update()
     {
+        // Jugador abre con E
         if (jugadorCerca && Keyboard.current.eKey.wasPressedThisFrame)
-        {
-            abierta = !abierta;
-            colliderBloqueo.enabled = !abierta;
-            navMeshObstacle.enabled = !abierta; // desactiva el obstáculo al abrir
-        }
+            Abrir();
 
         if (abierta)
             transform.rotation = Quaternion.Slerp(transform.rotation, rotacionAbierta, Time.deltaTime * velocidad);
@@ -34,15 +35,33 @@ public class AbrirPuerta : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, rotacionCerrada, Time.deltaTime * velocidad);
     }
 
+    public void Abrir()
+    {
+        abierta = true;
+        colliderBloqueo.enabled = false;
+        navMeshObstacle.enabled = false;
+    }
+
+    public void Cerrar()
+    {
+        abierta = false;
+        colliderBloqueo.enabled = true;
+        navMeshObstacle.enabled = true;
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-            jugadorCerca = true;
+        if (other.CompareTag("Player")) jugadorCerca = true;
+        if (other.CompareTag("Agente")) agenteCerca = true;
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-            jugadorCerca = false;
+        if (other.CompareTag("Player")) jugadorCerca = false;
+        if (other.CompareTag("Agente"))
+        {
+            agenteCerca = false;
+            Cerrar(); // cierra al salir el agente
+        }
     }
 }
