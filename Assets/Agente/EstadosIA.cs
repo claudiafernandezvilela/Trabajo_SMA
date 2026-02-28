3ef7487b4c2149ddf72f0fff98f73fc9afc09ce4
sofiagarciap <sofia.garciap@udc.es>
2026-02-28 16:59:04 +0100

    separar acciones de decisiones

--- Assets/Agente/EstadosIA.cs
+++ Assets/Agente/EstadosIA.cs
@@ -6,9 +6,6 @@ public class Patrulla : IEstado
     {
         if (!cerebro.agente.pathPending && cerebro.agente.remainingDistance < 0.5f)
             cerebro.SiguientePunto();
-
-        if (cerebro.JugadorVisible)
-            cerebro.CambiarEstado(new Perseguir());
     }
 }
 
@@ -17,87 +14,68 @@ public class Perseguir : IEstado
     public void Ejecutar(Cerebro cerebro)
     {
         if (cerebro.player != null)
+        {
             cerebro.agente.SetDestination(cerebro.player.position);
 
-        if (!cerebro.JugadorVisible)
-        {
-            cerebro.UltimaPosicion = cerebro.player.position;
-            cerebro.CambiarEstado(new Buscar());
-        }
+            float distancia = Vector3.Distance(
+                cerebro.transform.position,
+                cerebro.player.position
+            );
 
-        // Comprobar si alcanzó al jugador
-        if (cerebro.player != null)
-        {
-            float distancia = Vector3.Distance(cerebro.transform.position, cerebro.player.position);
             if (distancia <= cerebro.distanciaAtaque)
-            {
                 cerebro.AtraparJugador();
-            }
         }
     }
 }
 
 public class Buscar : IEstado
 {
-    private bool llegue = false;
+    private bool destinoAsignado = false;
     private float timer = 0f;
 
     public void Ejecutar(Cerebro cerebro)
     {
-        if (!llegue)
+        if (!destinoAsignado)
         {
             cerebro.agente.SetDestination(cerebro.UltimaPosicion);
-            llegue = true;
+            destinoAsignado = true;
+            return; // esperar al siguiente frame
         }
 
-        if (!cerebro.agente.pathPending && cerebro.agente.remainingDistance < 0.5f)
+        if (!cerebro.agente.pathPending &&
+            cerebro.agente.remainingDistance < 0.5f)
         {
-            timer += UnityEngine.Time.deltaTime;
-            if (timer >= cerebro.TiempoBusqueda)
-                // Si tiene objeto vigilado, ir a revisarlo 
-                // (ns si cambiarlo pq realmente todos los agentes tienen objeto asigando)
-                if (cerebro.objetoVigilado != null)
-                    cerebro.CambiarEstado(new RevisarObjeto());
-                else
-                    cerebro.CambiarEstado(new Patrulla());
-        }
+            timer += Time.deltaTime;
 
-        if (cerebro.JugadorVisible)
-            cerebro.CambiarEstado(new Perseguir());
+            if (timer >= cerebro.TiempoBusqueda)
+            {
+                cerebro.BusquedaTerminada();
+            }
+        }
     }
 }
+
+
 
 public class RevisarObjeto : IEstado
 {
-    private bool llegue = false;
+    private bool destinoAsignado = false;
 
     public void Ejecutar(Cerebro cerebro)
     {
-        if (!llegue)
+        if (!destinoAsignado)
         {
             cerebro.agente.SetDestination(cerebro.posicionObjeto);
-            llegue = true;
+            destinoAsignado = true;
+            return; // esperar al siguiente frame
         }
 
-        if (cerebro.JugadorVisible)
+        if (!cerebro.agente.pathPending &&
+            cerebro.agente.remainingDistance < 0.5f)
         {
-            cerebro.CambiarEstado(new Perseguir());
-            return;
-        }
-
-        if (!cerebro.agente.pathPending && cerebro.agente.remainingDistance < 0.5f)
-        {
-            if (Objeto.fueRecogido)
-            {
-                // El objeto desapareció, seguir buscando al ladrón
-                cerebro.CambiarEstado(new Buscar());
-            }
-            else
-            {
-                // El objeto sigue ahí, volver a patrullar
-                cerebro.CambiarEstado(new Patrulla());
-            }
+            cerebro.RevisionTerminada();
         }
     }
 }
+
 
